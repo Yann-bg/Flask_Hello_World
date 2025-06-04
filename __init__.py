3e94e7311070ff431008871c75bfd6da403a8034
Yann-bg <yannkueti@gmail.com>
2025-06-04 17:17:33 +0200

    Ajout compilation et deploiement

--- __init__.py
+++ __init__.py
@@ -46,4 +46,4 @@ def somme_valeurs(valeurs):
 
                                                                                                                
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
